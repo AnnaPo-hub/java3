0bdffe9f3c6411a6056247d847190a73c0ea1c59
Anna Popova <anna.popova201909@gmail.com>
2020-02-21 09:06:20 +0300

    java  file  added

---
+++ transfer.java
@@ -0,0 +1,11 @@
+public class transfer {
+    public static void main(String[] args) {
+        long currentBalance =  2_000_000_000;
+        long transferAmount = 500_000_000;
+        long total;
+        total=currentBalance+transferAmount;
+
+        System.out.println(total);
+
+    }
+}
